5763ddb90665a63230f7b08b631ad521989a26fa
BhupeshFarde <bhupeshfarde110@gmail.com>
2024-07-24 16:11:02 +0530

    react-ipl-point-table

--- react-to-do-app/src/App.js
+++ react-to-do-app/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import AllNotes from './Components/AllNotes';
+import NotesForm from './Components/NotesForm';
+import NavBar from './Components/NavBar'
+
+import { useState } from 'react';
+
+
 
 function App() {
+
+  const [mode,setMode]=useState('dark')
+
+  const toggleMode=()=>{
+    if(mode==='light')
+    {
+      setMode('dark')
+      document.body.style.backgroundColor='#30353b'
+    }
+    else
+    {
+      setMode('light')
+      document.body.style.backgroundColor='#d8e2ee'
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          
+      <BrowserRouter>
+      <NavBar mode={mode} toggleMode={toggleMode} />
+        <Routes>
+          <Route path='/' element={<NotesForm mode={mode} />} />
+          <Route path='/allNotes' element={<AllNotes mode={mode} />} />
+        </Routes>
+      </BrowserRouter>
+
+      
     </div>
   );
 }
